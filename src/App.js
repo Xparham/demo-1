9f0d1db6e198bfa4ec4c8ea937079ab4cb742daa
Xparham <xparham@gmail.com>
2021-02-03 18:12:34 -0500

    imported Midde file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Hero from './Hero'
+import Midde from './Midde';
 
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
+      <Midde/>
     </>
   );
 }
