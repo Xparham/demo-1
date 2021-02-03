b761e0de2a83519b4ec1541cd6680a4977e93837
Xparham <xparham@gmail.com>
2021-02-03 17:59:47 -0500

    imported code from job tracker

--- src/Midde.js
+++ src/Midde.js
@@ -1,7 +1,17 @@
 function Middle () {
     return (
         <>
-        
+            <hr>
+            <div class="flex flex-col">
+            <h1 class="flex text-5xl text-white text-center py-96 px-10" style="background-image: url(img/body-image2.png); background-repeat: no-repeat; background-size: cover;">Our various partnerships that allow our members access to hundreds of jobs in the Web Development/Software Tech Industry.</h1>
+            
+            
+            </div>
+
+            <div class="flex">
+                <img class="text-center w-1/2 h-1/2" src="img/body.jpg" alt="">
+                <img class="w-1/2"src="img/body-3.jpeg" alt="">
+            </div>
         
         
         
