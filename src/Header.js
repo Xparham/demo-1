3ea266f348d3a09c7629b67b9925c3721b57bba3
Xparham <xparham@gmail.com>
2021-02-03 16:09:07 -0500

    imorted LeftHeader

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
+import LeftHeader from "./LeftHeader";
+
+
+
 function Header () {
     return (
-        <div className="flex items-center justify-between px-6 py-6 mx-auto bg-black">
-            <h1 className="pl-6 text-2xl font-bold text-white lg:text-4xl">Gradus</h1>
-               
-              
-               
+        <div>
+           <LeftHeader/> 
+
         </div>
 
     );
