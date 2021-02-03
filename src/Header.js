6c2e8104da76ac56cd827b17cd79f4e80aa2f329
Xparham <xparham@gmail.com>
2021-02-03 14:52:54 -0500

    exported Header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header () {
+    return (
+        <div className="flex items-center justify-between px-6 py-6 mx-auto bg-black">
+            <h1 className="pl-6 text-2xl font-bold text-white lg:text-4xl">Gradus</h1>
+               
+              
+               
+        </div>
+
+    );
+}
+
+export default Header
