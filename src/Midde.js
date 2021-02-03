0a205330a210233c2f53417a6befeeaf876deafe
Xparham <xparham@gmail.com>
2021-02-03 18:03:39 -0500

    imported images

--- src/Midde.js
+++ src/Midde.js
@@ -1,9 +1,13 @@
+import Body from './body.jpg'
+import Body2 from './body-image2.png' 
+import Body3 from './body-3.jpeg'
+
 function Middle () {
     return (
         <>
             <hr>
             <div class="flex flex-col">
-            <h1 class="flex text-5xl text-white text-center py-96 px-10" style="background-image: url(img/body-image2.png); background-repeat: no-repeat; background-size: cover;">Our various partnerships that allow our members access to hundreds of jobs in the Web Development/Software Tech Industry.</h1>
+                <h1 class="flex text-5xl text-white text-center py-96 px-10" style="background-image: url(img/body-image2.png); background-repeat: no-repeat; background-size: cover;">Our various partnerships that allow our members access to hundreds of jobs in the Web Development/Software Tech Industry.</h1>
             
             
             </div>
