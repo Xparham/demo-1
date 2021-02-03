da4dcc15245d8cfc4a45907cc38ca73843c5d6fd
Xparham <xparham@gmail.com>
2021-02-03 16:08:19 -0500

    created function and exported

---
+++ src/LeftHeader.js
@@ -0,0 +1,9 @@
+function LeftHeader () {
+    return (
+        <div class="flex items-center justify-between px-6 py-6 mx-auto bg-black">
+            <h1 class="pl-6 text-2xl font-bold text-white lg:text-4xl">Gradus</h1>
+        </div>
+    )
+}
+
+export default LeftHeader;
