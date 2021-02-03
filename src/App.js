559938659258dcedde704dd26696e47d2af2b197
Xparham <xparham@gmail.com>
2021-02-03 18:26:19 -0500

    imported footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import Footer from './footer';
 import Header from './Header';
 import Hero from './Hero'
 import Midde from './Midde';
+import Footer from './Footer'
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header/>
       <Hero/>
       <Midde/>
+      <Footer/>
     </>
   );
 }
